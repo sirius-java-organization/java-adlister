fc1dd34a042cbbd049caedbdeb506d6768d8b7d8
kennethhowell <howell.kenneth.a@gmail.com>
2022-04-04 10:11:01 -0500

    morning ocmmit of changes

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,27 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        // if the name is null, it means that "name" was not present in the query
+        // string, and here we'll provide a default value
+        if (name == null) {
+            name = "World!";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+        // pass the value of the name variable to the view, and send the request
+        // forward to the hello.jsp file
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/differentHello.jsp").forward(request, response);
+
     }
+
 }
---
+++ src/main/webapp/differentHello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kennethhowell
+  Date: 4/1/22
+  Time: 9:16 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+I am different :D
+<h1>Hello there, ${name}!</h1>
+</body>
+</html>
